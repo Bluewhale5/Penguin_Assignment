cfe77cc04910fa46274054d3b3b8ca3a55d1a488
Bluewhale5 <117742839+Bluewhale5@users.noreply.github.com>
2022-12-06 12:42:35 +0000

    Create libraries.R

---
+++ PenguinProjects/functions/libraries.R
@@ -0,0 +1,9 @@
+
+#Libraries required for preparation, analysis and presentation of penguin data
+
+library(palmerpenguins)
+library(ggplot2)
+suppressPackageStartupMessages(library(janitor))
+suppressPackageStartupMessages(library(dplyr))
+library(ragg)
+library(svglite)
